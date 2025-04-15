20b79db54f2d4194b93e117af60e2197508de574
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-04-15 22:45:14 +0600

    resolve

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <div>
       <div className="navbar bg-base-100 shadow-sm">
@@ -85,4 +85,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default Navbar;
